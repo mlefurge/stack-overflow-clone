21e8dee86794d45bcee0d2e9a9688b1f1d195213
mlefurge <matthew.lefurge@gmail.com>
2015-12-04 20:43:18 -0500

    Add OAuth for FB and OAuth groundwork for Google

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'questions#index'
+
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
