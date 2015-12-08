2f7c3f7d3cff0ae730bc5ba8a3a148455346de10
mlefurge <matthew.lefurge@gmail.com>
2015-12-08 09:46:57 -0500

    Add user and question seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = []
+
+10.times do
+  users << User.create(provider: "Google", uid: Faker::Number.number(6), name: Faker::Name.name)
+end
+
+10.times do
+  users << User.create(provider: "Facebook", uid: Faker::Number.number(7), name: Faker::Name.name)
+end
+
+user.each do |user|
+  3.times do
+    Question.create(title: Faker::Haker.say_something_smart, user_id: user, content: Faker::Lorem.paragraphs(rand(1..4)))
+  end
+end
+
