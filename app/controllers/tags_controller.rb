67e73cf519c3bf3388ed0cd7add2aaae11133189
mlefurge <matthew.lefurge@gmail.com>
2015-12-09 10:56:14 -0500

    Remove unused routes from tags controller

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,18 +1,7 @@
 class TagsController < ApplicationController
 
-  def index
-    @tags = Tags.all
-  end
-
   def show
     @tag = Tag.find(params[:id])
   end
-
-  def new
-  end
-
-  def create
-  end
-
 
 end
