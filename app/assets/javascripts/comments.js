a97f89d77c25a70ea124f6baa02725eaab38a461
mlefurge <matthew.lefurge@gmail.com>
2015-12-09 16:20:22 -0500

    Ajax and style answer comments

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -13,4 +13,18 @@ $(document).ready(function() {
     $('#'+id+'.new_comment_button').toggle();
   })
 
+
+  $('.new_answer_comment_button').on('click', function(){
+    var id = $(this).attr('id')
+    $('#'+id+'.answer_show_new_comment_container').show();
+    $('#'+id+'.new_answer_comment_button').toggle();
+  });
+
+  $('.cancel_comment').on('click', function(event){
+    event.preventDefault();
+    var id = $(this).attr('id')
+    $('#'+id+'.answer_show_new_comment_container').toggle();
+    $('#'+id+'.new_answer_comment_button').toggle();
+  })
+
 })
