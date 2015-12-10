a71feb8a31ceb4faf1cf44c32f1afe6a4a8f3ebb
mlefurge <matthew.lefurge@gmail.com>
2015-12-10 17:32:20 -0500

    Add ruby method for top 15 tags. Query is inefficient, need to refactor

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,9 +1,18 @@
 class TagsController < ApplicationController
 
   def index
-    @tags = Tag.all
+    @sorted_tags = Tag.sort_tags(Tag.all)[0..14]
+    if params[:search]
+      search_tag = Tag.search(params[:search])
+      if search_tag.count == 0
+        @errors = ["There are no questions for the tag: #{params[:search]}"]
+        render :index
+      else
+        tag_id = search_tag.first.id
+        redirect_to tag_path(tag_id)
+      end
+    end
   end
-
 
   def show
     @tag = Tag.find(params[:id])
